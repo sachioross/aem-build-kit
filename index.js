cc4103bac65242926d2886629c303d6059d5ef1b
Douglas Ross <ross@adobe.com>
2016-12-25 21:51:56 -0800

    Initial commit - base configuration using pre-defined Bootstrap template

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-p, --port', 'Port')
+    .parse(process.argv);
+
+var port = 5502;
+
+if (program.port) {
+    port = program.port;
+}
+
+app.get('/', function(req, res) {
+   res.send('Working!');
+});
+
+app.use(express.static('static'));
+
+app.listen(port, function() {
+    console.log("Server started at " + new Date() + " on port " + port);
+});
