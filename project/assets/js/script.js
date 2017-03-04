ce17d9051896dd1553e87721d44c11ffcd945e9b
Douglas Ross <ross@adobe.com>
2017-03-03 22:32:02 -0600

    Adding basic script and style files for example / stubbed usage

---
+++ project/assets/js/script.js
@@ -0,0 +1,6 @@
+function printPurpose() {
+  return ["<div>","An AEM clientlib will create a script.js by default,",
+    "this file is a stub for the auto-generated script and should act as a ",
+    "starting point for creating customized client scripts","</div>"
+  ].join("");
+}
